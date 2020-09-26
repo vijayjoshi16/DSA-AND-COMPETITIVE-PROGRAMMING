755fb74101dd73dcf71add020f890c2a0f7d0692
vijayjoshi16 <joshivijay324@gmail.com>
2020-09-26 22:19:05 +0530

    Initial commit on Linked List

---
+++ Linked-List-GFG/06.ReverseLinkedListInGroupsOfGivenSize.cpp
@@ -0,0 +1,111 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct node *reverse (struct node *head, int k);
+
+struct node
+{
+    int data;
+    struct node* next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+/* UTILITY FUNCTIONS */
+/* Function to push a node */
+
+/* Function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+/* Drier program to test above function*/
+int main(void)
+{
+    /* Start with the empty list */
+
+     int t;
+     cin>>t;
+     
+     while(t--)
+     {
+         struct node* head = NULL;
+         struct node* temp = NULL;
+         int n;
+         cin >> n;
+         
+         for(int i = 0;i<n;i++){
+            int value;
+            cin >> value;
+            if(i == 0)
+            {
+                head = new node(value);
+                temp = head;
+            }
+            else
+            {
+                temp->next = new node(value);
+                temp = temp->next;
+            }
+         }
+        
+        int k;
+        cin>>k;
+        head = reverse(head, k);
+
+        printList(head);
+    
+     }
+     
+     return(0);
+
+}
+
+// } Driver Code Ends
+
+
+/*
+  Reverse a linked list
+  The input list will have at least one element  
+  Return the node which points to the head of the new LinkedList
+  Node is defined as 
+    struct node
+    {
+        int data;
+        struct node* next;
+    
+        node(int x){
+            data = x;
+            next = NULL;
+        }
+    
+    }*head;
+*/
+
+node *reverse (struct node *head, int k)
+{ 
+    node* curr=head;
+    node* prev=NULL;
+    node* n=NULL;
+    int ctr=k;
+    while(ctr--&&curr){
+        n=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=n;
+    }
+    if(n!=NULL)
+    head->next=reverse(n,k);
+    return prev;
+}
